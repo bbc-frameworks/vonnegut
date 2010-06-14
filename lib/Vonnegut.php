3b5d4ed118a91c743d819476be8468f66896e3d9
Pete Otaqui <pete.otaqui@bbc.co.uk>
2010-06-14 22:28:36 +0800

    refactored cli stuff out of Vonnegut, removed Vonnegut_Cli inheritance, made transacting over all code optional and not the default, added single file and file_list.txt operation. whew

--- lib/Vonnegut.php
+++ lib/Vonnegut.php
@@ -9,129 +9,33 @@
 class Vonnegut
 {
     
-    const LOG_LEVEL_CRITICAL = 1;
-    const LOG_LEVEL_WARN = 6;
-    const LOG_LEVEL_DEBUG = 12;
-    
-    /**
-     * Log level at which to pass calls to Vonnegut::log().
-     *
-     * @var string
-     */
-    public $log_level = self::LOG_LEVEL_WARN;
-    
-    /**
-     * Array of acceptable file extensions.
-     *
-     * @var string
-     */
-    public $types = array('php');
-    
-    /**
-     * Array of directories to iterate.
-     *
-     * @var string
-     */
-    protected $_directories = array();
-    
-    
-    /**
-     * Constructor function, if a directory path is 
-     * passed as the first argument, will run immediately.
-     *
-     * @param string $directory 
-     */
-    public function __construct($directory = null) {
-        if ( $directory !== null ) {
-            $this->addDirectory($directory);
-            $this->run();
-        }
-    }
-    
-    /**
-     * Adds a directory to the list to be iterated.
-     *
-     * @param string $directory 
-     * @return boolean
-     */
-    public function addDirectory($directory) {
-        if ( !is_dir($directory) ) {
-            $this->log("Invalid directory {$directory}!", Vonnegut::LOG_LEVEL_WARN);
-            return false;
-        }
-        array_push($this->_directories, $directory);
-        return true;
-    }
-    
-    /**
-     * Run the documentation generation routine.
-     *
-     * @return void
-     */
-    public function run() {
-        $list = array();
-        foreach ( $this->_directories as $dir ) {
-            $list += $this->reflectTree(new RecursiveDirectoryIterator($dir));
-        }
-        return $list;
-    }
-    
-    /**
-     * Recursive function to iterate a directory, and reflect valid files.
-     *
-     * @param RecursiveDirectoryIterator $iterator
-     * @return void
-     */
-    public function reflectTree($iterator, &$tree = array()) {
-        while ($iterator->valid()) {
-            if ($iterator->isDir() && !$iterator->isDot()) {
-                if ($iterator->hasChildren()) {
-                    $this->reflectTree($iterator->getChildren(), $tree);
-                }
-            }
-            else if ($iterator->isFile()) {
-                $path = $iterator->getPath() . '/' . $iterator->getFilename();
-                $pathinfo = pathinfo($path);
-                if (
-                    isset($pathinfo['extension']) &&
-                    in_array(strtolower($pathinfo['extension']), $this->types)
-                ) {
-                    $tree[$path] = $this->reflectFile($path);
-                }
-            }
-            $iterator->next();
-        }
-        return $tree;
-    }
-    
     /**
      * Reflects on a given file, parsing out classes and methods
      * for serialization.
      *
      * @param string $path the path of a file to reflect.
-     * @return object $doc the serialized documentation object.
+     * @return object the serialized documentation object.
      */
     public function reflectFile($path) {
-        $this->log("Reflecting file $path");
         require_once($path);
         $filename = (strpos($path,"/")!==false) ? preg_replace("|.*/(.+)$|",$path,"$1") : $path;
-        $doc = new StdClass();
-        $doc->path = $path;
-        $doc->classes = array();
+        $serial = new StdClass();
+        $serial->path = $path;
+        $serial->classes = array();
         $file_reflector = new Zend_Reflection_File($path);
         $classes = $file_reflector->getClasses();
         foreach ( $classes as $class ) {
-            $classOutput = $this->serializeDocBlockClass($class);
+            $classOutput = $this->reflectClass($class);
             $classOutput->methods = array();
             $methods = $class->getMethods();
             foreach ( $methods as $method ) {
                 if ( $method->getDeclaringClass()->name !== $class->name ) continue;
-                $methodOutput = $this->serializeDocBlockMethod($method);
+                $methodOutput = $this->reflectMethod($method);
                 array_push($classOutput->methods, $methodOutput);
             }
-            $doc->classes[] = $classOutput;
+            $serial->classes[] = $classOutput;
         }
-        return $doc;
+        return $serial;
     }
     
     /**
@@ -141,10 +45,9 @@ class Vonnegut
      * @return void
      * @author pete otaqui
      */
-    public function serializeDocBlockClass($reflection) {
+    public function reflectClass($reflection) {
         $serial = new StdClass();
         $serial->name = $reflection->name;
-            $this->log(" class ".$reflection->name);
             $properties = $reflection->getProperties();
             $serial->properties = array();
             foreach ( $properties as $property ) {
@@ -173,10 +76,9 @@ class Vonnegut
      * @return object
      * @author pete otaqui
      */
-    public function serializeDocBlockMethod($reflection) {
+    public function reflectMethod($reflection) {
         $serial = new StdClass();
         $serial->name = $reflection->name;
-        $this->log("  method ".$reflection->name);
         if ( $reflection->isPrivate() ) $serial->access = "private";
         if ( $reflection->isProtected() ) $serial->access = "protected";
         if ( $reflection->isPublic() ) $serial->access = "public";
@@ -207,19 +109,6 @@ class Vonnegut
         }
         return $serial;
     }
-    
-    
-    
-    /**
-     * Print a log message;
-     */
-    public function log($str,$level=null) {
-        if ( $level == null ) $level = self::LOG_LEVEL_DEBUG;
-        if ( $level <= $this->log_level ) {
-            print $str;
-        }
-    }
-    
     
     
 }
--- lib/Vonnegut/Cli.php
+++ lib/Vonnegut/Cli.php
@@ -7,94 +7,267 @@
  * @package Vonnegut
  * @author pete otaqui
  **/
-class Vonnegut_Cli extends Vonnegut
+class Vonnegut_Cli
 {
     
     
+    const LOG_LEVEL_CRITICAL = 1;
+    const LOG_LEVEL_WARN = 6;
+    const LOG_LEVEL_DEBUG = 12;
+    
+    /**
+     * Instance of the vonnegut class.
+     *
+     * @var object
+     */
+    protected $_vonnegut;
+    
+    /**
+     * Array of acceptable file extensions.
+     *
+     * @var array
+     */
+    public $_fileTypes = array('php');
+    
+    
+    /**
+     * Log level at which to pass calls to Vonnegut_Cli::log().
+     *
+     * @var string
+     */
+    public $log_level = self::LOG_LEVEL_WARN;
+    
+    /**
+     * Array of acceptable output formats.
+     * 
+     * @var array
+     */
     protected $_formats = array('json');
+    
+    /**
+     * Output format, @see $_formats
+     *
+     * @var string
+     */
     protected $_format = "json";
+    
+    /**
+     * Ask for confirmation, default is TRUE
+     *
+     * @var boolean
+     */
     protected $_ask = true;
+    
+    /**
+     * Array of files to reflect.
+     * 
+     * @var array
+     */
+    protected $_files = array();
+    
+    /**
+     * Running in single file mode.
+     * 
+     * @var $boolean
+     */
+    protected $_singleFile = false;
+    
+    /**
+     * Transact generation (try and read all files before 
+     * writing any output)
+     *
+     * @var boolean
+     **/
+    protected $_transact = false;
+    
+    /**
+     * Directory or file in which to write output.
+     *
+     * @var string
+     **/
+    protected $_outputPath;
+    
     
     /**
      * Constructor function, will attempt to parse
      * command line options & arguments and then run.
      *
-     * @param string $directory 
      */
     public function __construct() {
+        $this->_vonnegut = new Vonnegut();
         global $argc, $argv;
         $args = $this->parseArgs($argv);
-        if ( !isset($args[1]) ) {
+        if ( !isset($args[0]) || isset($args['h']) ) {
             $this->usage($argv[0]);
             $this->_exit();
         } else {
             $this->_ask = ( $args['y'] ) ? false : true;
-            if ( isset($args['quiet']) ) $this->log_level = Vonnegut::LOG_LEVEL_CRITICAL;
-            if ( isset($args['v']) ) $this->log_level = Vonnegut::LOG_LEVEL_DEBUG;
+            if ( isset($args['quiet']) ) $this->log_level = Vonnegut_Cli::LOG_LEVEL_CRITICAL;
+            if ( isset($args['v']) ) $this->log_level = Vonnegut_Cli::LOG_LEVEL_DEBUG;
             if ( isset($args['f']) ) {
                 if ( !in_array($args['f'], $this->_formats) ) {
                     $this->_exit(1, "Invalid format '{$args['f']}' specified!");
                 }
                 $this->_format = $args['f'];
             }
-            $this->addDirectory($args[0]);
-            $this->_outputDirectory = $args[1];
-            if ( !is_dir($this->_outputDirectory) ) {
-                if ( $this->_ask ) {
-                    $this->log("Output Directory '{$this->_outputDirectory}' does not exist.  Create it? y/n [n]", Vonnegut::LOG_LEVEL_CRITICAL);
-                    $yesno = trim(strtolower(fgets(STDIN)));
-                    $make_directory = ($yesno == "y" || $yesno == "yes");
+            if ( is_dir($args[0]) ) {
+                $this->addDirectory($args[0]);
+            } elseif ( is_file($args[0]) ) {
+                if ( $this->_isPhpFile($args[0]) ) {
+                    $this->addFile($args[0]);
+                    $this->_singleFile = true;
                 } else {
-                    $make_directory = true;
-                }
-                if ( $make_directory ) {
-                    $made_directory = mkdir($this->_outputDirectory);
-                    if ( $made_directory == false ) {
-                        $this->_exit(1, "Could not make directory {$this->_outputDirectory}!");
+                    $file = fopen($args[0],'r');
+                    while ( !feof($file) ) {
+                        $buffer = fgets($file);
+                        $this->addFile($buffer);
                     }
-                } else {
-                    $this->_exit(1, 'Exiting - output directory doesn\'t exist and won\'t be created.');
                 }
             }
-            $tree = $this->run();
-            $this->_outputTree($tree);
+            
+            if ( isset($args['o']) ) {
+                if ( !is_file($args['o']) && !is_dir($args['o']) ) {
+                    $this->_exit(1, 'Exiting - output path doesn\'t exist.');
+                }
+                $this->_outputPath = $args['o'];
+            }
+            
+            if ( isset($args['t']) ) {
+                $this->_transact = true;
+            }
+            
+            $reflections = $this->reflectFiles();
         }
     }
     
-    /**
-     * Outputs the parsed tree of documentation serializations.
-     * 
-     * @param object $tree 
-     * @return void
-     */
-    protected function _outputTree($tree) {
-        foreach ( $tree as $index=>$item ) {
-            $this->_outputTreeItem($index, $item);
-        }
-    }
     
     /**
-     * undocumented function
+     * Adds a directory to the list to be iterated.
      *
-     * @param object $item 
+     * @param string $directory 
+     * @return boolean
+     */
+    public function addDirectory($directory) {
+        if ( !is_dir($directory) ) {
+            $this->log("Invalid directory {$directory}!", Vonnegut_Cli::LOG_LEVEL_WARN);
+            return false;
+        } else {
+            $this->_recurseDirectory(new RecursiveDirectoryIterator($directory));
+        }
+    }
+    protected function _recurseDirectory($iterator) {
+        while ($iterator->valid()) {
+            if ($iterator->isDir() && !$iterator->isDot()) {
+                if ($iterator->hasChildren()) {
+                    $this->_recurseDirectory($iterator->getChildren());
+                }
+            } elseif ($iterator->isFile()) {
+                $path = $iterator->getPath() . '/' . $iterator->getFilename();
+                $pathinfo = pathinfo($path);
+                if ( $this->_isPhpFile($path) ) {
+                    $this->addFile($path);
+                }
+            }
+            $iterator->next();
+        }
+    }
+    
+    /**
+     * Adds a file to the list to be iterated.
+     *
+     * @param string $file 
+     * @return boolean
+     */
+    public function addFile($file) {
+        if ( !is_file($file) || !$this->_isPhpFile($file) ) {
+            $this->log("Invalid file {$file}!", Vonnegut_Cli::LOG_LEVEL_WARN);
+            return false;
+        }
+        $this->_files[] = $file;
+        return true;
+    }
+    
+    /**
+     * Reflects an array of files.  If no argument provided, will
+     * use $this->_files;
+     * 
+     * @param array $files the array of files to relfect (optional)
+     * @return array $reflections
+     */
+    public function reflectFiles($files = null) {
+        if ( $files == null ) $files = $this->_files;
+        $reflections = array();
+        foreach ( $files as $file ) {
+            $this->log("Reading $file");
+            $reflection = $this->_vonnegut->reflectFile($file);
+            if ( !$this->_transact ) {
+                $this->_outputReflectionFile($file, $reflection);
+            }
+            $reflections[$file] = $reflection;
+        }
+        if ( $this->_transact ) {
+            $this->_outputReflectionFiles($reflections);
+        }
+        return $reflections;
+    }
+    
+    
+    
+    /**
+     * Outputs a set of reflections.
+     * 
+     * @param array $reflections array in the form (filename=>reflection)
      * @return void
      */
-    protected function _outputTreeItem($index, $item) {
-        //$this->log("$index");
-        //print_r($item);
-        // May need Vonnegut_Output_Json and Vonnegut_Output_Otherformat
-        // classes in the future.
+    protected function _outputReflectionFiles($reflections) {
+        foreach ($reflections as $infile => $reflection) {
+            $this->_outputReflectionFile($infile, $reflection);
+        }
+    }
+    
+    /**
+     * Outputs the reflection - can be to the command line or a file,
+     * or a directory.
+     *
+     * @param string $infile
+     * @param object $reflection 
+     * @return void
+     */
+    protected function _outputReflectionFile($infile, $reflection) {
+        // May need Vonnegut_Output_Json and Vonnegut_Output_Otherformat classes
+        // in the future.
         if ( $this->_format == 'json' ) {
-            $filename = str_replace(DIRECTORY_SEPARATOR, '_', $index);
-            $filepath = $this->_outputDirectory . $filename . '.json';
-            $this->log("Writing $filepath");
-            $file = fopen($filepath,'w');
-            $json = Zend_Json::encode($item);
-            $rote = fwrite($file, $json);
-            if ( $rote === false ) {
-                $this->log("Could not write $filename", Vonnegut::LOG_LEVEL_WARN);
+            if ( $this->_outputPath ) {
+                if ( $this->_singleFileMode ) {
+                    $filepath = $this->_outputPath;
+                } else {
+                    $filename = str_replace(DIRECTORY_SEPARATOR, '_', $infile);
+                    $filepath = $this->_outputPath . $filename . '.json';
+                }
+                $this->log("Writing $filepath");
+                $file = fopen($filepath,'w');
+                $json = Zend_Json::encode($reflection);
+                $rote = fwrite($file, $json);
+                if ( $rote === false ) {
+                    $this->log("Could not write $filename", Vonnegut_Cli::LOG_LEVEL_WARN);
+                }
             }
         }
+    }
+    
+    /**
+     * Checks for a PHP file by using the extension.
+     *
+     * @param string $path 
+     * @return boolean
+     */
+    protected function _isPhpFile($path) {
+        $pathinfo = pathinfo($path);
+        $isPhpFile = (
+            isset($pathinfo['extension']) &&
+            in_array(strtolower($pathinfo['extension']), $this->_fileTypes)
+        );
+        return $isPhpFile;
     }
     
     /**
@@ -103,14 +276,20 @@ class Vonnegut_Cli extends Vonnegut
     public function usage($filename) {
         $usage = <<<USAGE
 Vonnegut PHP docblock parser command line interface
-Usage : {$filename} [options] /path/to/php/files /path/to/output
+Usage : {$filename} [options] /tree/of/php/files/
+        {$filename} [options] /single/php/file.php
+        {$filename} [options] /one/php/file/per/line.txt
 
- -f <format>  Output <format>, currently only 'json' is supported
- -v           Verbose console output
- -y           Respond 'yes' to input queries
+ -f <format>  Output <format>, currently only 'json' is supported.
+ -h           Print help (this message) and exit.
+ -o <path>    Write output to <path>.  Should be a directory if
+              reflecting multiple files.  Default is STDOUT.
+ -t           "Transaction" - try to parse all files before outputting.
+ -v           Verbose console output.
+ -y           Respond 'yes' to input queries.
 
 USAGE;
-        $this->log($usage, Vonnegut::LOG_LEVEL_CRITICAL);
+        $this->log($usage, Vonnegut_Cli::LOG_LEVEL_CRITICAL);
     }
 
     /**
@@ -118,7 +297,10 @@ USAGE;
      */
     public function log($str, $level = null) {
         $str .= "\n";
-        parent::log($str, $level);
+        if ( $level == null ) $level = self::LOG_LEVEL_DEBUG;
+        if ( $level <= $this->log_level ) {
+            print $str;
+        }
     }
     
     /**
@@ -131,7 +313,7 @@ USAGE;
     protected function _exit( $code=0, $message=null, $logLevel=null )
     {
         if ( $message !== null ) {
-            if ( $code !== 0 && $logLevel == null ) $logLevel = Vonnegut::LOG_LEVEL_CRITICAL;
+            if ( $code !== 0 && $logLevel == null ) $logLevel = Vonnegut_Cli::LOG_LEVEL_CRITICAL;
             $this->log($message, $logLevel);
         }
         exit($code);
